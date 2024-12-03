0e58ab520a2067a51a1fecba2beeb1cfb0a03842
Alex <aivaylovivanov@gmail.com>
2024-12-03 15:47:26 +0000

    day3 part2 done

--- day3/sol3.py
+++ day3/sol3.py
@@ -4,19 +4,27 @@ import re
 
 ## Part 1 of Day 3 ## 
 
-# regex pattern to match requirements for mul(x,y)
-pattern = r"mul\((-?\d+),(-?\d+)\)"
+def findMul(s):
+    # regex pattern to match requirements for mul(x,y)
+    pattern = r"mul\((\d+),(\d+)\)"
 
-# find all matches in the string
-matches = re.findall(pattern, puzzleInput)
+    # find all matches in the string
+    matches = re.findall(pattern, s)
 
-total = 0 
+    total = 0 
 
-for x in matches: 
-    total += (int(x[0]) * int(x[1]))
+    for x in matches: 
+        total += (int(x[0]) * int(x[1]))
 
-print(total)
+    return total
 
 ## Part 2 of Day 3 ## 
 
+doPuzzle = puzzleInput.split("do()")
 
+total = 0 
+
+for x in doPuzzle: 
+    total += findMul(x.split("don't()")[0])
+
+print(total)
